934c33b775f6230608b4d1f54166d06d4724d2ea
oneofthezombies <hunhoekim@gmail.com>
2025-06-06 18:51:58 -0400

    Add Google font integration and update index page content
    - Enhanced astro.config.mjs to include Google font provider for "EB Garamond".
    - Modified Base.astro layout to preload the new font and apply it to the body.
    - Updated index.mdx to change the heading from "Hello" to "Hello World" and added additional headings for improved content structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   adapter: vercel(),
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "EB Garamond",
+        cssVariable: "--font-eb-garamond",
+      },
+    ],
+  },
 });
