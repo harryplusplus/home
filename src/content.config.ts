dd69fd7b267ad60725e7b3783f07b7a0b217dd58
oneofthezombies <hunhoekim@gmail.com>
2025-06-07 02:04:41 -0400

    Remove description field from blog post schema and individual posts for cleaner content structure

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,6 @@ const blog = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/blog" }),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     pubDate: z.date(),
   }),
 });
