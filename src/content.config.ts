0b773d36c8d590e534d64fc6eb6e304aaacae236
oneofthezombies <hunhoekim@gmail.com>
2025-06-08 22:04:47 -0400

    Add projects section and update blog content structure
    - Introduced a new projects collection in content configuration, allowing for better organization of project-related content.
    - Updated blog loader paths to reflect new directory structure.
    - Deleted outdated blog post and added new blog entries with relevant titles and publication dates.
    - Enhanced the header component to include a link to the new projects section for improved navigation.

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,19 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/blog" }),
+  loader: glob({ pattern: "**/*.mdx", base: "./src/contents/blog" }),
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
   }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/contents/projects" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+  }),
+});
+
+export const collections = { blog, projects };
