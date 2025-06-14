fed2e7a830015330a734e5293719c97716d4b7cd
oneofthezombies <hunhoekim@gmail.com>
2025-06-13 21:30:08 -0400

    Remove React and Mermaid Dependencies from Astro Configuration
    - Eliminated the React integration and rehype-mermaid plugin from the Astro configuration to streamline the project setup.
    - Updated package.json and package-lock.json to reflect the removal of related dependencies, enhancing project simplicity and maintainability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,15 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import { defineConfig, fontProviders } from "astro/config";
-import rehypeMermaid from "rehype-mermaid";
 import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx({
-      syntaxHighlight: {
-        type: "shiki",
-        excludeLangs: ["mermaid"],
-      },
       remarkPlugins: [remarkToc],
-      rehypePlugins: [rehypeMermaid],
     }),
-    react(),
   ],
   adapter: vercel(),
   experimental: {
